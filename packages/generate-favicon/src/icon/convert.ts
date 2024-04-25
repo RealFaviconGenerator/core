ea5b759d4d7cfceb873e8f3edfaaeda4c22ad59d
Philippe Bernard <philippe.bernard@gmail.com>
2024-04-25 23:40:35 +0200

    Remove startDownload

--- packages/generate-favicon/src/icon/convert.ts
+++ packages/generate-favicon/src/icon/convert.ts
@@ -7,19 +7,6 @@ export const rawDataToDownloadableContent = (content: string, mimeType: string):
   'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(content)
 );
 
-export const startDownload = (content: string, fileName: string) => {
-  var element = document.createElement('a');
-  element.setAttribute('href', content);
-  element.setAttribute('download', fileName);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-};
-
 export const stringToSvg = (svg: string, imageAdapter: ImageAdapter): Svg => {
   const s = imageAdapter.createSvg();
   s.svg(svg);
