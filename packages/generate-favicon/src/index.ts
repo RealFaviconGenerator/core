359aeeee116a5eb4df528c069bd2ad843ca6c348
Philippe Bernard <philippe.bernard@gmail.com>
2024-04-25 23:40:57 +0200

    Export several functions and types

--- packages/generate-favicon/src/index.ts
+++ packages/generate-favicon/src/index.ts
@@ -2,3 +2,40 @@
 export {
   ImageAdapter,
 } from '@/svg/adapter';
+
+export {
+  bitmapToSvg, stringToSvg,
+  anyUrlToSvg, dataUrlToSvg
+} from '@/svg/index';
+
+export {
+  DesktopIconSettings, initDesktopIconSettings
+} from '@/icon/desktop';
+
+export {
+  generateDesktopFaviconHtml, generateDesktopFaviconFiles
+} from '@/generation/desktop';
+
+export {
+  TouchIconSettings, initTouchIconSettings
+} from '@/icon/ios';
+
+export {
+  generateTouchIconHtml, generateTouchIconFiles
+} from '@/generation/touch-icon';
+
+export {
+  WebAppManifestSettings, initWebAppManifestSettings
+} from '@/icon/web-app-manifest';
+
+export {
+  generateWebAppManifestHtml, generateWebAppManifestIconFiles
+} from '@/generation/web-app-manifest';
+
+export {
+  FaviconIconSettings, FaviconSettings, initFaviconIconSettings
+} from '@/icon/index';
+
+export {
+  normalizeFaviconPath
+} from '@/generation/index';
