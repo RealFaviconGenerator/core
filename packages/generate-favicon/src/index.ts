9c419db61688cc6a52a547b15f78a1e74de1e920
Philippe Bernard <philippe.bernard@gmail.com>
2024-04-26 14:02:58 +0200

    Don't use alias path

--- packages/generate-favicon/src/index.ts
+++ packages/generate-favicon/src/index.ts
@@ -1,41 +1,41 @@
 
 export {
   ImageAdapter,
-} from '@/svg/adapter';
+} from './svg/adapter';
 
 export {
   bitmapToSvg, stringToSvg,
   anyUrlToSvg, dataUrlToSvg
-} from '@/svg/index';
+} from './svg/index';
 
 export {
   DesktopIconSettings, initDesktopIconSettings
-} from '@/icon/desktop';
+} from './icon/desktop';
 
 export {
   generateDesktopFaviconHtml, generateDesktopFaviconFiles
-} from '@/generation/desktop';
+} from './generation/desktop';
 
 export {
   TouchIconSettings, initTouchIconSettings
-} from '@/icon/ios';
+} from './icon/ios';
 
 export {
   generateTouchIconHtml, generateTouchIconFiles
-} from '@/generation/touch-icon';
+} from './generation/touch-icon';
 
 export {
   WebAppManifestSettings, initWebAppManifestSettings
-} from '@/icon/web-app-manifest';
+} from './icon/web-app-manifest';
 
 export {
   generateWebAppManifestHtml, generateWebAppManifestIconFiles
-} from '@/generation/web-app-manifest';
+} from './generation/web-app-manifest';
 
 export {
   FaviconIconSettings, FaviconSettings, initFaviconIconSettings
-} from '@/icon/index';
+} from './icon/index';
 
 export {
   normalizeFaviconPath
-} from '@/generation/index';
+} from './generation/index';
