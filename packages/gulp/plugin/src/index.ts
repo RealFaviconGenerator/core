00e68cb0abf706881a59f869ce5669222ff30c6d
Philippe Bernard <philippe.bernard@gmail.com>
2024-05-20 19:21:06 +0200

    Add Gulp plugin boilerplate

---
+++ packages/gulp/plugin/src/index.ts
@@ -0,0 +1,5 @@
+
+export { generateFavicon } from './generate-favicon';
+export { injectFaviconMarkups } from './inject-favicon-markups';
+export { checkForUpdates } from './check-for-updates';
+export { escapeJSONSpecialChars } from './escape-json-special-chars';
