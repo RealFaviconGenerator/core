4f03367c20cdabd89e5b08f1f7e9d072d3b54722
Philippe Bernard <philippe.bernard@gmail.com>
2024-12-10 19:48:05 +0100

    Sample background with gradient

--- packages/generate-logo/src/index.ts
+++ packages/generate-logo/src/index.ts
@@ -21,11 +21,12 @@ const scale = 0.9;
   const newRefY = ((1 - textScale) * textHeight) / 2;
   const centerX = -newRefX - bbox.x1 + (imageWidth - (textWidth * textScale)) / 2;
   const centerY = -newRefY - bbox.y1 + (imageHeight - (textHeight * textScale)) / 2;
+
   const adapter = await getNodeImageAdapter();
   const svg = adapter.createSvg();
   svg.viewbox(0, 0, imageWidth, imageHeight);
 
-  const rect = svg.rect(imageWidth, imageHeight).fill('white').stroke('black');
+  const rect = svg.rect(imageWidth, imageHeight).fill("url('#gradient')");
 
   const centeredGroup = svg.group();
   centeredGroup
@@ -35,6 +36,34 @@ const scale = 0.9;
     .fill('black')
     .scale(textScale)
     .translate(centerX, centerY);
+
+  const defs = svg.defs();
+  const gradient = defs.element('linearGradient', {
+    id: 'gradient',
+    gradientTransform: 'rotate(10)'
+  });
+  gradient.element('stop', {
+    offset: '0%',
+    'stop-color': '#FFD700'
+  });
+  gradient.element('stop', {
+    offset: '100%',
+    'stop-color': '#FF8C00'
+  });
+
+  const emojiGroup = svg.group();
+  const emoji = emojiGroup.text('😊').font({ size: 100 }).center(imageWidth / 2, imageHeight / 2);
+  // const emojiBbox = emojiGroup.rbox();
+  // const emojiScale = 8;
+  // const emojiNewRefX = ((1 - textScale) * emojiBbox.width) / 2;
+  // const emojiNewRefY = ((1 - textScale) * emojiBbox.height) / 2;
+  // const emojiCenterX = -emojiNewRefX - emojiBbox.x + (imageWidth - (emojiBbox.width * emojiScale)) / 2;
+  // const emojiCenterY = -emojiNewRefY - emojiBbox.y + (imageHeight - (emojiBbox.height * emojiScale)) / 2;
+  // emojiGroup.scale(emojiScale).translate(emojiCenterX, emojiCenterY);
+  // console.log("emojiBbox", emojiBbox);
+  // const emojiCenterX = (imageWidth - emojiBbox.width) / 2 - emojiBbox.x;
+  // const emojiCenterY = (imageHeight - emojiBbox.height) / 2 - emojiBbox.y;
+  // emojiGroup.translate(emojiCenterX, emojiCenterY);
 
   fs.writeFileSync('logo.svg', svg.svg());
 })();
