05d182a1e125fb80b687b4e4583f9e2dded59b16
Philippe Bernard <philippe.bernard@gmail.com>
2024-12-18 20:14:15 +0100

    More imports in sandbox for easier quick typing

--- packages/sandbox/index.ts
+++ packages/sandbox/index.ts
@@ -1,5 +1,7 @@
 import { generateFaviconFiles } from '@realfavicongenerator/generate-favicon';
 import { getNodeImageAdapter } from "@realfavicongenerator/image-adapter-node";
+import { generateLogo} from '@realfavicongenerator/generate-logo';
+import fs from 'fs';
 
 (async () => {
   console.log("Hello!");
